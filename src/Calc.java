08ca1e882ff30462e75b9560618b45e1c4a6527d
krizzlan <stulen.christian@gmail.com>
2020-11-03 11:18:31 +0100

    Some error handling and you can now input which unit uses what weapon attacking what unit.

--- src/Calc.java
+++ src/Calc.java
@@ -2,20 +2,32 @@ import javax.swing.*;
 
 public class Calc {
 
-public static void ranged(int nrOfAttacker, Unit attacker, Weapon weapon, Unit defender) {
+public static void ranged(int nrOfAttacker, Unit attacker, Weapon weapon, Unit defender,boolean isRanged) {
     double hits;
     double wounds;
     double damage;
+    int toHit;
+    int attacks;
 
-    hits = nrOfAttacker * weapon.attack * (7-attacker.ballisticSkill)/6.;
-    System.out.println("Hits: "+hits);
+    Unit[] calcArray = new Unit[2];
+    calcArray[0]=attacker;
+    calcArray[1]=defender;
+    if (isRanged) {
+        toHit = calcArray[0].ballisticSkill;
+        attacks=weapon.attack;
+    }
+    else {
+        toHit = calcArray[0].weaponSkill;
+        attacks=calcArray[0].attack;
+        calcArray[0].strength =+weapon.strength;
+    }
 
+    hits = nrOfAttacker * attacks * (7-toHit)/6.;
+    //System.out.println("Hits: "+hits);
     wounds = (((7-strVsTou(weapon.strength,defender.toughness))/6.)*hits);
-    System.out.println("Wounds: "+wounds);
-
+    //System.out.println("Wounds: "+wounds);
     damage = wounds* (1-(totalArmorSave(weapon.armorPenetration, defender.armorSave))/6.);
-    System.out.println("Damage: "+damage);
-
+    //System.out.println("Damage: "+damage);
     JOptionPane.showMessageDialog(null,String.format("Total damage: %.2f D",damage*weapon.damage));
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,38 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        Unit[] unitsArray = new Unit[2];
-        Weapon[] weaponsArray = new Weapon[3];
-        int input=0;
+        Unit[] unitsArray= new Unit[0];
+        Weapon[] weaponsArray= new Weapon[0];
 
+        int input;
         int nextUnit=0;
         int nextWeapon=0;
+        int choice;
+        int nrOfAttackers;
+        int attacker;
+        int defender;
+        int weaponChoice=0;
+        boolean isRanged;
         boolean isRunning=true;
         while (isRunning){
             try {
-                input = Integer.parseInt(JOptionPane.showInputDialog("Välj meny" +
+                input = Integer.parseInt(JOptionPane.showInputDialog("Choose an option:" +
                         "\n1: Add Units " +
                         "\n2: Add weapons" +
                         "\n3: Calculate probability" +
                         "\n4: Exit"));
-                if (input>4) errorMessage("Please typ a number, 1-4");
+                if (input>4) errorMessage("Please typ an integer, 1-4");
             }catch (Exception e1) {
-                errorMessage("Please typ a number, 1-4");
+                input=0;
+                errorMessage("Please typ an integer, 1-4");
             }
             switch (input) {
                 case 1 -> {
+                    if (unitsArray.length>0){
+                        errorMessage("Units already added.");
+                        break;
+                    }
+                    unitsArray = new Unit[2];
                     unitsArray[nextUnit] = new Unit("Guard",4,4,3,3,1,1,5);
                     nextUnit++;
                     unitsArray[nextUnit] = new Unit("Space Marine",3,3,4,4,2,2,3);
@@ -31,6 +43,13 @@ public class Main {
 
                 }
                 case 2 -> {
+                    if (weaponsArray.length>0){
+                        errorMessage("Weapons already added.");
+                        break;
+                    }
+                    weaponsArray = new Weapon[4];
+                    weaponsArray[nextWeapon] = new Weapon("Powersword","melee",1,2,1,3);
+                    nextWeapon++;
                     weaponsArray[nextWeapon] = new Weapon("Lasgun","ranged",3,1,1,0);
                     nextWeapon++;
                     weaponsArray[nextWeapon] = new Weapon("Bolter","ranged",4,1,2,1);
@@ -41,10 +60,52 @@ public class Main {
                     }
                 }
                 case 3 -> {
-                    try {
-                        Calc.ranged(10, unitsArray[0], weaponsArray[0], unitsArray[1]);
+                    if (weaponsArray.length==0 || unitsArray.length==0){
+                        errorMessage("No Unit and/or weapon has been added.");
+                        break;
                     }
-                    catch (Exception e2) {errorMessage("No Unit and/or weapon has been added.");}
+                    try{
+                    nrOfAttackers = Integer.parseInt(JOptionPane.showInputDialog("How many attackers? 1-100: "));
+                    if (nrOfAttackers>100 || nrOfAttackers<1) {
+                        errorMessage("Please type an integer, 1-100");
+                        break;}
+
+                    attacker= Integer.parseInt(JOptionPane.showInputDialog("Who are attacking? \n0:Guard\n1:Space Marine"));
+                    if (attacker>unitsArray.length || attacker<0) {
+                        errorMessage("Please choose 0 or 1");
+                        break;
+                    }
+                    defender= Integer.parseInt(JOptionPane.showInputDialog("Who are defending? " +
+                            "\n0:Guard" +
+                            "\n1:Space Marine"));
+                    if (defender>unitsArray.length || defender<0) {
+                            errorMessage("Please choose 0 or 1");
+                            break;
+                    }
+
+                    choice = JOptionPane.showConfirmDialog(null,"Is it a ranged attack?");
+                        //System.out.println(choice);
+                    if (choice==0) {
+                        isRanged=true;
+                        weaponChoice = Integer.parseInt(JOptionPane.showInputDialog("Which weapon are they wielding? " +
+                                "\n1:Lasgun" +
+                                "\n2:Bolter" +
+                                "\n3:Plasmagun"));
+                        if (weaponChoice>weaponsArray.length || weaponChoice<0) {
+                            errorMessage("Please choose 1,2 or 3");
+                            break;
+                        }
+                    }
+                    else if (choice==1) isRanged=false;
+                    else break;
+
+
+                    Calc.ranged(nrOfAttackers, unitsArray[attacker], weaponsArray[weaponChoice], unitsArray[defender],isRanged);
+                    }
+                    catch (Exception e3){
+                        errorMessage("Please type an integer.");
+                    }
+
                 }
                 case 4 -> isRunning = false;
 
